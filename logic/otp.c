ae4847430a535cf18c219830542ca7a52fb089a9
Andrew Walbran <qwandor@google.com>
2011-09-26 17:49:00 +0100

    Adding Google copyright notice.

--- logic/otp.c
+++ logic/otp.c
@@ -1,5 +1,6 @@
 /**
     Copyright (c) 2011 Yohanes Nugroho (yohanes@gmail.com)
+    Copyright (c) 2011 Google Inc. (qwandor@google.com)
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
